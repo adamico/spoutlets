b127f63bd617c23d68bec8b146dcc843a584ac78
Don Nguyen <don@unswrc.com>
2012-10-24 10:11:17 +1100

    #8 - Personal screen is now shown

---
+++ spoutlets.js
@@ -0,0 +1,26 @@
+if (Meteor.isClient) {
+    
+/**
+  Meteor.loginWithPassword = function(user, password, function() {
+      console.log('done');
+  });
+
+
+  Meteor.loginWithGithub({
+    requestPermissions: ['user', 'public_repo']
+  }, function (err) {
+      console.log('callback');
+    if (err)
+      Session.set('errorMessage', err.reason || 'Unknown error');
+    else
+      console.log('GitHub success');
+  });
+*/
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+    console.log('server starting');
+  });
+}
